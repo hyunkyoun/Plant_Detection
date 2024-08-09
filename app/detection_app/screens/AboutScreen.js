4cfabb6716ed8dc10a16384280234397b0c1d303
hyunkyoun <ekim2350@gmail.com>
2024-08-09 14:19:10 -0400

    .

--- app/detection_app/screens/AboutScreen.js
+++ app/detection_app/screens/AboutScreen.js
@@ -1,8 +1,30 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import axios from 'axios';
 
 const AboutScreen = ({navigation}) => {
+
+  const [inputData, setInputData] = useState('');
+  const [responseData, setResponseData] = useState(null);
+
+  // setInputData("input data");
+
+  const sendData = () => {
+    try {
+      const res = axios.post('http://localhost:5001/api/process', { key1: "value1", key2: "value2" })
+        .then((res) => {
+          console.log(res);
+          setResponseData(res.data);
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,6 +39,14 @@ const AboutScreen = ({navigation}) => {
       {/* text in the about screen */}
       <View style={styles.text_container}>
         <Text>This is the about screen!</Text>
+      </View>
+      <View style={styles.button_container}>
+        <Button 
+          onPress={sendData}
+          title="Press"
+        />
+        <Text>Response: </Text>
+        <Text>{JSON.stringify(responseData)}</Text>
       </View>
     </View>
   );
@@ -35,6 +65,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  button_container: {
+    flex: 1, 
+  }, 
 });
 
 export default AboutScreen;
